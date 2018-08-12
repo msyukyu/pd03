c1c08e2bbaeed96ee4899990d14d40de7e3a5b4e
Damien Abeloos <dabeloos@e-r2-p6.s19.be>
2018-08-12 14:14:17 +0200

    finished

--- ex09/ft_sort_integer_table.c
+++ ex09/ft_sort_integer_table.c
@@ -6,53 +6,30 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/12 08:59:05 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/12 12:29:00 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/12 14:14:00 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_partition(int *tab, int start, int end)
-{
-	int		s;
-	int		i;
-	int		j;
-
-	i = start - 1;
-	j = start;
-	while (j < end)
-	{
-		if (tab[j] < tab[end])
-		{
-			i = i + 1;
-			s = tab[j];
-			tab[j] = tab[i];
-			tab[i] = s;
-		}
-	}
-	s = tab[i + 1];
-	tab[i + 1] = tab[end];
-	tab[end] = s;
-	return i + 1;
-}
-
-void	ft_quicksort_integer_table(int *tab, int start, int end)
-{
-	int		m;
-
-	if (start < end)
-	{
-		m = ft_partition_integer_table(tab, start, end);
-		ft_quicksort_integer_table(tab, start, m - 1);
-		ft_quicksort_integer_table(tab, m + 1, end);
-	}
-}
-
 void	ft_sort_integer_table(int *tab, int size)
 {
-	ft_quicksort_integer_table(tab, 0, size - 1);
-}
+	int		i;
+	int		j;
+	int		buffer;
 
-int		main(void)
-{
-	int tab
-	return (0);
+	j = size;
+	while (j > 1)
+	{
+		i = 0;
+		while (i + 1 < j)
+		{
+			buffer = tab[i];
+			if (buffer > tab[i + 1])
+			{
+				tab[i] = tab[i + 1];
+				tab[i + 1] = buffer;
+			}
+			i++;
+		}
+		j--;
+	}	
 }
