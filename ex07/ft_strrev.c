83df557e8097ec93f695f16664575f3223d5425d
Damien Abeloos <dabeloos@w-r1-p1.s19.be>
2018-08-12 08:39:46 +0200

    ex07 ft_strrev

--- ex07/ft_strrev.c
+++ ex07/ft_strrev.c
@@ -6,51 +6,29 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/12 04:43:11 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/12 05:42:24 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/12 08:38:54 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include <stdio.h>
 
 char	*ft_strrev(char *str)
 {
 	int		l;
 	int		i;
 	char	c;
-	char	*s;
-	char	*e;
 
 	l = 0;
-	*e = *str;
-	*s = *str;
 	while (str[l] != '\0')
 	{
-		printf("%d", i);
 		l++;
-		e++;
 	}
-	e--;
 	l--;
 	i = 0;
-	while (i < l / 2)
+	while (i < (l + 1) / 2)
 	{
-		c = *e;
-		*e = *s;
-		*s = c;
+		c = str[l - i];
+		str[l - i] = str[i];
+		str[i] = c;
 		i++;
-		s++;
-		e--;
 	}
 	return (str);
-}
-
-int		main(void)
-{
-	char *str = "abcde";
-
-	printf("%s", str);
-	str = ft_strrev(str);
-	printf("%c", '\n');
-	printf("%s", str);
-	return (0);
 }
