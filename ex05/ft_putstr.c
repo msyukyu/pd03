d8f946cb638ecaabb7706ef6d78444ebcca77d8e
Damien Abeloos <dabeloos@w-r1-p1.s19.be>
2018-08-12 08:47:49 +0200

    correction ex05

--- ex05/ft_putstr.c
+++ ex05/ft_putstr.c
@@ -6,7 +6,7 @@
 /*   By: dabeloos <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/10 14:08:35 by dabeloos          #+#    #+#             */
-/*   Updated: 2018/08/12 04:28:51 by dabeloos         ###   ########.fr       */
+/*   Updated: 2018/08/12 08:47:10 by dabeloos         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ void	ft_putstr(char *str)
 {
 	while (*str != '\0')
 	{
-		ft_putchar(*str + '\0');
+		ft_putchar(*str);
 		str++;
 	}
 }
